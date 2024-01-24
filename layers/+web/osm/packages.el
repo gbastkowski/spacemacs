657da0c223c06d59283805c5b94676a97544355d
Gunnar Bastkowski <gunnar.bastkowski@mobimeo.com>
2024-01-24 13:35:57 +0100

    Add osm layer

---
+++ layers/+web/osm/packages.el
@@ -0,0 +1,28 @@
+(defconst osm-packages '(osm))
+
+(defun osm/init-osm ()
+  (use-package osm
+    :defer t
+    :init
+    (progn
+      (add-to-list 'evil-buffer-regexps '("\\*osm\\*" . normal))
+      (spacemacs/declare-prefix "aO" "OpenStreetMap")
+      (spacemacs/set-leader-keys "aOh" 'osm-home)
+      (spacemacs/set-leader-keys "aOg" 'osm-goto))
+    :config
+    (progn
+      ;; (define-key osm-keymap "h" 'osm-left)
+      (spacemacs/set-leader-keys-for-major-mode 'osm-mode
+        "bD" 'osm-bookmark-delete
+        "bs" 'osm-bookmark-set
+        "bj" 'osm-bookmark-jump)
+      (evil-define-key 'normal osm-mode-map
+        "/" 'osm-search
+        "h" 'osm-left
+        "j" 'osm-down
+        "k" 'osm-up
+        "l" 'osm-right
+        "s" 'osm-server
+        "=" 'osm-zoom-in
+        "+" 'osm-zoom-in
+        "-" 'osm-zoom-out))))
